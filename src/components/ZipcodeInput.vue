ee9bb89040ddb2a71026b3aea2a4bada9833194a
irinazivitz <irina.woodward@gmail.com>
2024-12-20 10:10:30 -0500

    fixed icons

--- src/components/ZipcodeInput.vue
+++ src/components/ZipcodeInput.vue
@@ -9,7 +9,15 @@
             v-model="zipcode" />
         <div class ="d-flex align-items-center" >
             <button class ="btn" type="submit" style="background-color: #2B4560">
-                <i class= "fas fa-search" style="color: #e1e7e0"></i>
+                <!-- <i class= "fas fa-search" style="color: #e1e7e0"></i> -->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" fill="#e1e7e0" stroke="#e1e7e0" stroke-width="15">
+                    <path d="M505 442.7L405.3 343c28.4-34.8 45.7-79.5 
+                    45.7-128.3 0-111-90.3-201.3-201.3-201.3S48.4 103.7 
+                    48.4 214.7 138.7 416 249.7 416c48.8 0 93.5-17.3 
+                    128.3-45.7l99.7 99.7c9.3 9.3 24.4 9.3 33.7 0 9.4-9.3 
+                    9.4-24.4 0-33.7zM80.4 214.7c0-93.2 76-169.3 169.3-169.3S419 
+                    121.5 419 214.7 343 384 249.7 384c-93.2 0-169.3-76.1-169.3-169.3z"/>
+                </svg>
             </button>
             <!-- <i  v-if = " weatherStore.isMobile" 
                 class = "fas fa-cog ms-2" 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from 'vue';
 import { createPinia} from 'pinia';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const app = createApp(App);
 const pinia = createPinia();
